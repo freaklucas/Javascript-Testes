126fe2963041ead17253ad67acb365c9bbd47c26
freaklucas <lucas.oliveira.freak@gmail.com>
2021-11-06 16:53:04 -0300

    teste de queryString ok

--- projeto1/src/lib/queryString.spec.js
+++ projeto1/src/lib/queryString.spec.js
@@ -1,15 +1,38 @@
-const { queryString } = require("./queryString");
+const { queryString } = require('./queryString');
 
-describe("Object to query string", () => {
-  it("should create a valid query string when an object is provided", () => {
+describe('Object to query string', () => {
+  it('should create a valid query string when an object is provided', () => {
     const obj = {
-      name: "Lucas",
-      profession: "developer",
+      name: 'Lucas',
+      profession: 'developer',
     };
 
     queryString(obj);
-    expect(queryString(obj)).toBe("name=Lucas&profession=developer");
+    expect(queryString(obj)).toBe('name=Lucas&profession=developer');
   });
+});
+
+it('should create a valid query string even when an array is passed as value', () => {
+  const obj = {
+    name: 'Lucas',
+    abilities: ['JS', 'Vue.js'],
+  };
+
+  expect(queryString(obj)).toBe('name=Lucas&abilities=JS,Vue.js');
+});
+
+it('should throw an error when an object is passed as value', () => {
+  const obj = {
+    name: 'Lucas',
+    abilities: {
+      first: 'JS',
+      second: 'Vue.js',
+    },
+  };
+
+  expect(() => {
+    queryString(obj);
+  }).toThrowError();
 });
 
 // describe('Query string to object', () => {
