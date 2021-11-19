4876bf65a45a51a7be066fdde6b1981beb45fc05
freaklucas <lucas.oliveira.freak@gmail.com>
2021-11-18 23:32:23 -0300

    organizando getTotal como bloco

--- projeto1/src/lib/Cart.spec.js
+++ projeto1/src/lib/Cart.spec.js
@@ -15,41 +15,44 @@ describe('Cart ', () => {
   beforeEach(() => {
     cart = new Cart();
   });
-  it('should return 0 when getTotal() is executed in a newly created instance', () => {
-    expect(cart.getTotal()).toEqual(0);
-  });
 
-  it('should multiply quantify and price and receive the total amount', () => {
-    expect(cart.getTotal()).toEqual(0);
-  });
-
-  it('should ensure no more than on product exists at a time', () => {
-    cart.add({
-      product,
-      quantity: 2,
+  describe('getTotal', () => {
+    it('should return 0 when getTotal() is executed in a newly created instance', () => {
+      expect(cart.getTotal()).toEqual(0);
     });
 
-    cart.add({
-      product,
-      quantity: 1,
+    it('should multiply quantify and price and receive the total amount', () => {
+      expect(cart.getTotal()).toEqual(0);
     });
 
-    expect(cart.getTotal()).toEqual(35388);
-  });
+    it('should ensure no more than on product exists at a time', () => {
+      cart.add({
+        product,
+        quantity: 2,
+      });
 
-  it('should update total when a product gets includded and the removed', () => {
-    cart.add({
-      product,
-      quantity: 2,
+      cart.add({
+        product,
+        quantity: 1,
+      });
+
+      expect(cart.getTotal()).toEqual(35388);
     });
 
-    cart.add({
-      product: product2,
-      quantity: 1,
+    it('should update total when a product gets includded and the removed', () => {
+      cart.add({
+        product,
+        quantity: 2,
+      });
+
+      cart.add({
+        product: product2,
+        quantity: 1,
+      });
+
+      cart.remove(product);
+
+      expect(cart.getTotal()).toEqual(41872);
     });
-
-    cart.remove(product);
-
-    expect(cart.getTotal()).toEqual(41872);
   });
 });
